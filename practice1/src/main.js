f698ab618f4f6c5b0af0363a8fa1d00af07d270f
taquaki-satwo <satoreco@gmail.com>
2017-05-11 16:12:47 +0900

    bind style

--- practice1/src/main.js
+++ practice1/src/main.js
@@ -1,23 +1,13 @@
 import Vue from 'vue'
-import axios from 'axios'
-import _ from 'lodash'
 
 const app = new Vue({ // eslint-disable-line no-unuserd-vars
   el: '#app',
   data: {
-    isActive: true,
-    error: null,
-    classObject: {
-      active: true,
-      'text-danger': null
-    }
-  },
-  computed: {
-    classObj: function () {
-      return {
-        active: this.isActive && !this.error,
-        'text-danger': this.error && this.error.type === 'fatal',
-      }
+    activeColor: 'blue',
+    fontSize: 30,
+    styleObject: {
+      color: 'green',
+      fontSize: '40px'
     }
   }
 })
