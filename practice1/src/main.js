ddcb0664219b7b197c43cc8961c07a1295e255e8
taquaki-satwo <satoreco@gmail.com>
2017-05-11 15:42:18 +0900

    class object

--- practice1/src/main.js
+++ practice1/src/main.js
@@ -5,36 +5,19 @@ import _ from 'lodash'
 const app = new Vue({ // eslint-disable-line no-unuserd-vars
   el: '#app',
   data: {
-    q: '',
-    a: '入力してください'
-  },
-  watch: {
-    q: function(newQ) {
-      this.a = '入力中'
-      this.get()
+    isActive: true,
+    error: null,
+    classObject: {
+      active: true,
+      'text-danger': null
     }
   },
-  methods: {
-    get: _.debounce(
-      function () {
-        if (this.q.indexOf('?') === -1) {
-          this.a = '?を入力してください'
-          return
-        }
-        this.a = '考え中'
-        const vm = this
-        axios.get('https://yesno.wtf/api')
-          .then(function (res) {
-            vm.a = _.capitalize(res.data.answer)
-          })
-          .catch(function (error) {
-            vm.a = `Error ${error}`
-          })
-      },
-      500
-    )
+  computed: {
+    classObj: function () {
+      return {
+        active: this.isActive && !this.error,
+        'text-danger': this.error && this.error.type === 'fatal',
+      }
+    }
   }
 })
-
-console.log(vm.fullName)
-console.log(vm.fullName = 'aaa bbb')
