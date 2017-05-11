a567a3e8366dd10f618c045bae82aad1fabca0b0
taquaki-satwo <satoreco@gmail.com>
2017-05-11 16:44:13 +0900

    v-if

--- practice1/src/main.js
+++ practice1/src/main.js
@@ -1,13 +1,4 @@
 import Vue from 'vue'
-
 const app = new Vue({ // eslint-disable-line no-unuserd-vars
-  el: '#app',
-  data: {
-    activeColor: 'blue',
-    fontSize: 30,
-    styleObject: {
-      color: 'green',
-      fontSize: '40px'
-    }
-  }
+  el: '#app'
 })
