c5f4934b3c36275f0cd4a7b591dbdc156bbc5b5d
taquaki-satwo <satoreco@gmail.com>
2017-05-11 14:31:08 +0900

    watch option

--- practice1/src/main.js
+++ practice1/src/main.js
@@ -1,18 +1,40 @@
 import Vue from 'vue'
+import axios from 'axios'
+import _ from 'lodash'
 
-const vm = new Vue({ // eslint-disable-line no-unuserd-vars
+const app = new Vue({ // eslint-disable-line no-unuserd-vars
   el: '#app',
   data: {
-    msg: 'hello'
+    q: '',
+    a: '入力してください'
   },
-  computed: {
-    reversedMsg: function () {
-      return this.msg.split('').reverse().join('')
+  watch: {
+    q: function(newQ) {
+      this.a = '入力中'
+      this.get()
     }
+  },
+  methods: {
+    get: _.debounce(
+      function () {
+        if (this.q.indexOf('?') === -1) {
+          this.a = '?を入力してください'
+          return
+        }
+        this.a = '考え中'
+        const vm = this
+        axios.get('https://yesno.wtf/api')
+          .then(function (res) {
+            vm.a = _.capitalize(res.data.answer)
+          })
+          .catch(function (error) {
+            vm.a = `Error ${error}`
+          })
+      },
+      500
+    )
   }
 })
 
-vm.$watch('a', function (newVal, oldVal) {
-  console.log(newVal)
-  console.log(oldVal)
-})
+console.log(vm.fullName)
+console.log(vm.fullName = 'aaa bbb')
