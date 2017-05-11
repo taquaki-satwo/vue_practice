9c95e6570597250721aa4556bc50637dccbae76f
taquaki-satwo <satoreco@gmail.com>
2017-05-11 19:30:06 +0900

    fiter

--- practice1/src/main.js
+++ practice1/src/main.js
@@ -1,29 +1,22 @@
 import Vue from 'vue'
 
-Vue.component('todo-item', {
-  template: `
-    <li>
-      {{title}}
-      <button @click="$emit('remove')">X</button>
-    </li>
-  `,
-  props: ['title']
-})
-
 const app = new Vue({ // eslint-disable-line no-unuserd-vars
   el: '#app',
   data: {
-    newTodoText: '',
-    todos: [
-      'todo1',
-      'todo2',
-      'todo3'
-    ]
+    numbers: [1, 2, 3, 4, 5]
+  },
+  computed: {
+    evenNumbers: function () {
+      return this.numbers.filter(function (number) {
+        return number % 2 === 0
+      })
+    }
   },
   methods: {
-    addNewTodo: function () {
-      this.todos.push(this.newTodoText)
-      this.newTodoText = ''
+    even: function (numbers) {
+      return numbers.filter(function (number) {
+        return number % 2 === 0
+      })
     }
   }
 })
