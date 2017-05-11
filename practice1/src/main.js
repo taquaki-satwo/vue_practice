6722d10571278d3d71d888697f67c211309df05a
taquaki-satwo <satoreco@gmail.com>
2017-05-11 17:35:32 +0900

    v-for

--- practice1/src/main.js
+++ practice1/src/main.js
@@ -2,5 +2,15 @@ import Vue from 'vue'
 const app = new Vue({ // eslint-disable-line no-unuserd-vars
   el: '#app',
   data: {
+    parentMsg: 'vue',
+    items: [
+      { msg: 'hello' },
+      { msg: 'world' }
+    ],
+    object: {
+      first: 'john',
+      last: 'Doe',
+      age: 30
+    }
   }
 })
