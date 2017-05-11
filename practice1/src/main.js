5ec5cfa27b5a71fe02361795ffcdac5ab4882221
taquaki-satwo <satoreco@gmail.com>
2017-05-11 22:12:47 +0900

    event

--- practice1/src/main.js
+++ practice1/src/main.js
@@ -3,20 +3,25 @@ import Vue from 'vue'
 const app = new Vue({ // eslint-disable-line no-unuserd-vars
   el: '#app',
   data: {
-    numbers: [1, 2, 3, 4, 5]
-  },
-  computed: {
-    evenNumbers: function () {
-      return this.numbers.filter(function (number) {
-        return number % 2 === 0
-      })
-    }
+    counter: 10,
+    name: 'vue.js'
   },
   methods: {
-    even: function (numbers) {
-      return numbers.filter(function (number) {
-        return number % 2 === 0
-      })
+    greet: function (e) {
+      alert('Hello' + this.name)
+      if (e) {
+        alert(e.target.tagName)
+      }
+    },
+    addCounter: function () {
+      this.counter += 1
+    },
+    say: function(msg) {
+      alert(msg)
+    },
+    warm: function(msg, e) {
+      if (e) e.preventDefault()
+      alert(msg + ' ' + e.currentTarget)
     }
   }
 })
