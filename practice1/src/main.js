e5bd818c0d74eb3ef4abf25813954376db631d3d
taquaki-satwo <satoreco@gmail.com>
2017-05-12 09:35:21 +0900

    $emit

--- practice1/src/main.js
+++ practice1/src/main.js
@@ -1,45 +1,28 @@
 import Vue from 'vue'
 
-Vue.component('global-component', {
-  template: '<div>Global component</div>'
-})
-
-const LocalComponent = {
-  props: ['myMessage'],
-  template: '<div>Local component {{myMessage}}</div>',
-}
-
-Vue.component('data-function', {
-  template: '<button @click="counter += 1">{{counter}}</button>',
+Vue.component('button-counter', {
+  template: '<button v-on:click="increment">{{ counter }}</button>',
   data: function () {
     return {
       counter: 0
     }
-  }
-})
-
-Vue.component('local-property', {
-  props: ['localMsg'],
-  template: '<div>{{localMsg}} & {{msg}} & {{lowerCase}}</div>',
-  data: function () {
-    return {
-      msg: this.localMsg
-    }
   },
-  computed: {
-    lowerCase: function () {
-      return this.msg.trim().toLowerCase()
+  methods: {
+    increment: function () {
+      this.counter += 1
+      this.$emit('cal')
     }
   }
 })
 
 const vm = new Vue({ // eslint-disable-line no-unuserd-vars
   el: '#app',
-  components: {
-    'local-component': LocalComponent
-  },
   data: {
-    myMessage: '文字テンプレート',
-    parentMsg: 'Parent message'
+    total: 0
+  },
+  methods: {
+    incrementTotal: function () {
+      this.total += 1
+    }
   }
 })
